e37f5d5ec094513f54516455bdb11ea5f7bc9192
BananaRey <37335483+BananaRey@users.noreply.github.com>
2018-05-25 00:59:53 +0000

    Update ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -6,8 +6,37 @@
 # con sigma = 10, beta=2.67, rho=28.0,
 # condiciones iniciales t=0, x=0.0, y=0.0, z=0.0, hasta t=5.0.
 # Prepare dos graficas con la solucion: de x vs y (xy.png), x vs. z (xz.png) 
+import numpy as np
+import matplotlib.pyplot as plt
+
+h=0.1
+N=5.0/0.1
+x=np.zeros(50)
+y=np.zeros(50)
+z=np.zeros(50)
+def dx_dt(x,y,z):
+    return(10.0*(y-z))
 
 
+def dy_dt(x,y,z):
+    return(28.0*x-y-x*z)
+
+
+def dz_dt(x,y,z):
+    return(-2.67*z+x*y)
+
+
+for i in range(len(x)-1):
+    x[i+1]=x[i]+h*(dx_dt(x[i],y[i],z[i]))
+    y[i+1]=x[i]+h*(dy_dt(x[i],y[i],z[i]))
+    z[i+1]=x[i]+h*(dz_dt(x[i],y[i],z[i]))
+
+plt.ply(x,y)
+plt.savefig("xy.png")
+plt.close()
+plt.ply(x,z)
+plt.savefig("xz.png")
+plt.close()
 
 
 
