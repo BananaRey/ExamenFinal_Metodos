4da96e048c58a9b2904b63037bc77cebb58433d7
BananaRey <37335483+BananaRey@users.noreply.github.com>
2018-05-25 00:48:00 +0000

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,9 +4,13 @@
 # Guarde esta segunda derivada en funcion de x en una grafica llamada 'segunda.png'
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 x = np.linspace(0,2.,10)
 fx = np.array([0., 0.0494, 0.1975, 0.4444, 0.7901,1.2346 , 1.7778, 2.4198, 3.1605, 4.])
-
-
+df_dx=(fx[1:]-fx[:-1])/0.2
+x_2=x[1:]
+print(x_2,df_dx)
+plt.plot(x_2,df_dx)
+plt.savefig("segunda.png")
 
